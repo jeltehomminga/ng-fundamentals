5c5d77dd47a836e121ebee7eeb6a3e3109c6782f
Jelte Homminga <jeltehomminga@gmail.com>
2018-08-02 21:55:33 +0200

    Initial commit events app

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { EventsAppComponent } from "./events-app.component";
+import { EventsListComponent } from "./events/events-list.component";
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [EventsAppComponent, EventsListComponent],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule {}
