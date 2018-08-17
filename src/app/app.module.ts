527499bb30615de08674a9d7341f7e19dee8b737
Homminga Jelte (YIANB) <UAB923@eur.corp.vattenfall.com>
2018-08-17 15:39:13 +0200

    create reusable service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { EventsAppComponent } from "./events-app.component";
-import { EventsListComponent } from "./events/events-list.component";
-import { EventsThumbnailComponent } from "./events/events-thumbnail.component";
-import { NavBarComponent } from "./nav/navbar.component";
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventsThumbnailComponent } from './events/events-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
 @NgModule({
   imports: [BrowserModule],
   declarations: [
@@ -13,6 +14,7 @@ import { NavBarComponent } from "./nav/navbar.component";
     EventsThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
